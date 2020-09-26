b35b0f138a6d9b897329a5e207d5d7b31c52d225
Arturas <a.molcanovas@gmail.com>
2020-09-26 02:07:12 +0100

    refactor: Use numeric internal event names

--- projects/core/common/ReduxOMTEvent.ts
+++ projects/core/common/ReduxOMTEvent.ts
@@ -1,8 +1,8 @@
 /** @internal */
 export const enum ReduxOMTEvent { // eslint-disable-line no-shadow
-  ACTION_PROCESSED = 'romt:action-processed',
-  ACTION_DISPATCHED = 'romt:dispatched',
-  INITIAL_STATE = 'romt:initial-state',
-  READY = 'romt:ready',
-  INITIAL_STATE_REQUEST = 'romt:rq-initial-state'
+  ACTION_PROCESSED,
+  ACTION_DISPATCHED,
+  INITIAL_STATE,
+  READY,
+  INITIAL_STATE_REQUEST
 }
